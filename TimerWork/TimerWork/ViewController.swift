ef661cd5fa8b773727fc47d331440694b14132c4
Medya <33479784+medyahan@users.noreply.github.com>
2024-12-01 18:33:07 +0300

    Upload

---
+++ TimerWork/TimerWork/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  TimerWork
+//
+//  Created by Medya Han on 1.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = Timer()
+    var counter = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counter = 10
+        timeLabel.text = "Time: \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+    }
+
+    @objc func timerFunction() {
+        
+        timeLabel.text = "Time: \(counter)"
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timeLabel.text = "Time's Over"
+        }
+       
+    }
+
+}
+
---
+++ TimerWork/TimerWorkTests/TimerWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerWorkTests.swift
+//  TimerWorkTests
+//
+//  Created by Medya Han on 1.12.2024.
+//
+
+import Testing
+@testable import TimerWork
+
+struct TimerWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
